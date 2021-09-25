0031047b3a65cb6d4fce741ac79b4bf37657580d
jonmolinna <jmolina2624@gmail.com>
2021-09-25 11:59:39 -0500

    Break y Continue

---
+++ 1.-logica-programacion/break-continue.js
@@ -0,0 +1,15 @@
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+for(let i=0; i < numeros.length; i++){
+    if(i===5){
+        break;
+    }
+    console.log(numeros[i]);
+}
+
+for(let i=0; i < numeros.length; i++){
+    if(i===5){
+        continue;
+    }
+    console.log(numeros[i]);
+}
