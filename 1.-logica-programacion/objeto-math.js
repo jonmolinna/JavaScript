dd72248262f1acbdf6b8d489fb2ee7674d11156e
jonmolinna <jmolina2624@gmail.com>
2021-09-26 18:02:11 -0500

    Objeto Math

---
+++ 1.-logica-programacion/objeto-math.js
@@ -0,0 +1,27 @@
+console.log(Math);
+
+// Valor PI
+console.log(Math.PI);
+
+// Valores Absoluto (Cantidad de Numero)
+console.log(Math.abs(-7.8));
+
+// Valor redondeo
+console.log(Math.ceil(7.2)); // 8
+console.log(Math.floor(7.8)); // 7
+console.log(Math.round(7.2)); // 7
+console.log(Math.round(7.8)); // 8
+
+// Raiz Cuadrado
+console.log(Math.sqrt(81)); // 9
+
+// Potencia
+console.log(Math.pow(2, 6)); // 64
+
+// Indica si el numero positivo o negativo
+console.log(Math.sign(9)); // 1
+console.log(Math.sign(-3)); // -1
+
+// Numero Aleatorio (0-1)
+console.log(Math.random()); // 0.053794097029252086
+console.log(Math.round(Math.random() * 100)); // 0 - 100
