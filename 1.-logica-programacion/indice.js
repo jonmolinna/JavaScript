200e321221a4acfd2ded88bfcccbee426c2773a2
jonmolinna <jmolina2624@gmail.com>
2021-10-09 10:38:11 -0500

    Logica de Programación

---
+++ 1.-logica-programacion/indice.js
@@ -0,0 +1,89 @@
+/*
+1. Variables: var - let
+    var => variable goblal
+    let => variable en bloque o local (Usar let como variables)
+
+2. Constantes: (const)
+    Los constantes se usan para variables que no van a cambiar su contenido (solo en variables primitivos).
+    el objetos y arreglos si cambian.
+
+3. Cadenas de Texto (Strings)
+    link => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/String
+    let nombre = "Jon";
+    let saludo = new String("Hola Mundo");
+
+4. Template Strings
+    let nombre = "Jon"
+    let apellido = "Dallas"
+    let saludo = "Hola " + nombre + " " + apellido
+    let saludo = `Hola ${nombre} ${apellido}`;
+
+5. Números (Numbers)
+    link => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Number
+    let a = 2;
+    let b = 7.19
+    b.toFixed(2) => da formato a numeros decimales
+
+6. Booleans
+    let verdadero = true;
+    let falso = false;
+
+7. undefined, null & NaN
+    undefined indica que no se ha inicializado una vaariable y que el valor esta ausente.
+    null es valor especial que indica la ausencia de un valor
+
+8. Funciones
+
+9. Arreglos
+    link => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+10. Objetos
+    link => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Object
+
+11. Tipos de Operadores
+    - Aritmeticos
+        modulo 5%2 = 1
+    - Relacionales => >, <, >=, <=, ==, ===, !=, !==
+    - Logicos => ! - Not, || - Or, && - And
+
+12. Condicionales.
+
+13. Ciclos (Loops).
+
+14. Manejo de Errores.
+
+15. Break y Continue.
+
+16. Destructuración
+
+17. Objetos Literales
+
+18. Parámetros REST y Operador Spread
+
+19. Arrow Functions
+
+20. Prototipos (POO)
+
+21. Herencia Prototípica (POO)
+
+22. Clases y Herencia (POO)
+
+23. Métodos estáticos, getters y setters (POO)
+
+24. Objeto console
+
+25. Objeto Date
+
+26. Objeto Math
+
+27. Operadores de Cortocircuitos
+
+28. alert, confirm y prompt
+
+29. Expresiones Regulares
+
+30. Funciones Anónimas Autoejecutables
+
+31. Módulos ( import / export )
+
+*/
